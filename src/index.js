9dbdbd611238c1ce30a7f40a215ac832c4916375
Giovane Kauer <gfkauer@gmail.com>
2022-02-17 19:54:13 +0000

    lesson 16

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 
 import './index.css'
 import Primeiro from './componets/basic/Primero'
+import ComParamatro from './componets/basic/ComParamatro'
 
 ReactDOM.render(
   <div>
     <Primeiro />
+    <ComParamatro
+      titulo="Primeiro titulo"
+      aluno="Pedro Silva"
+      nota={9.5}
+    />
   </div>,
   document.getElementById('root')
 );
