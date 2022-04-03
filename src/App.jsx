c45f526c2afdb8bc7e5ffd89b7e72ba94a5fb6e6
Giovane Kauer <gfkauer@gmail.com>
2022-04-03 13:55:03 +0000

    manual resync project

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-import './App.css'
+import Card from "./components/layout/Card";
 
-import Primeiro from './componets/basic/Primero';
-import ComParametro from './componets/basic/ComParamatro';
-import Fragmento from './componets/basic/Framento';
-import Random from './componets/basic/Random';
-import Card from './componets/layout/Card'
+// import ListaAlunos from "./components/repeticao/ListaAlunos";
+// import Familia from "./components/basic/Familia";
+// import FamiliaMembro from "./components/basic/FamiliaMembro";
+// import Aleatorio from "./components/basic/Random";
+// import Fragmento from "./components/basic/Fragmento";
+// import ComParametro from "./components/basic/ComParametro";
+import Primeiro from "./components/basic/Primeiro";
 
-// TODO: remover este trecho
-// eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
     <div className="App">
-        <h1>Fundamentos React</h1>
-        <div className='Cards'>
-            <Card titulo="#04 Desafio Aleatório">
-                <Random max={100} min={5} />
+        <h1>Fundamentos React!</h1>
+
+        <div className="Cards">
+
+            {/* <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos></ListaAlunos>
             </Card>
-            <Card titulo="#03 Desafio Aleatório">
+
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Silva">
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
+                </Familia>
+            </Card>
+
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
-
             </Card>
-            <Card titulo="#02 Desafio Aleatório">
+
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                 <ComParametro
-                    titulo="Primeiro titulo"
+                    titulo="Situação do Aluno"
                     aluno="Pedro Silva"
-                    nota={9.5}
+                    nota={9.3}
                 />
-            </Card>
-            <Card titulo="#01 Desafio Aleatório">
+            </Card> */}
 
-                <Primeiro />
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
             </Card>
         </div>
     </div>
--- src/components/basic/Random.jsx
+++ src/components/basic/Random.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default function (props) {
     return (
         <div>
